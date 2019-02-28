28465745291f6cf03253ad56efa19150af87eb8c
Sihem Meriki <sihemmeriki2@gmail.com>
2019-02-28 11:22:04 -0500

    product order quantity

--- server/db/models/product-order.js
+++ server/db/models/product-order.js
@@ -1,16 +1,29 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Product_Order = db.define('product_order', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+const Product_Order = db.define(
+  'product_order',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    quantity: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0
+    }
   },
-  quantity: {
-    type: Sequelize.INTEGER,
-    defaultValue: 0
+  {
+    hooks: {
+      beforeCreate: quantity => quantity > 0
+    }
+  }
+)
+
+Product_Order.beforeCreate(function(quantity) {
+  if (quantity < 1) {
+    throw new Error('You cannot have 0 quantity')
   }
 })
-
 module.exports = Product_Order
