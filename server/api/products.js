eaf848f8f9ef7893c0d7c832e3795b8ae39797c0
erpanken <44857046+erpanken@users.noreply.github.com>
2019-02-28 12:25:01 -0500

    feature (routes) we now have a route to get all products by category and to get a single product (#68)

--- server/api/products.js
+++ server/api/products.js
@@ -2,16 +2,17 @@ const router = require('express').Router()
 const {Product} = require('../db/models')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+router.get('/:category', async (req, res, next) => {
   try {
-    const products = await Product.findAll()
-    res.json(products)
+    const category = req.params.category
+    const productsByCategory = await Product.findAll({where: {category}})
+    res.json(productsByCategory)
   } catch (error) {
     next(error)
   }
 })
 
-router.get('/:productId', async (req, res, next) => {
+router.get('/:category/:productId', async (req, res, next) => {
   try {
     const id = req.params.productId
     const product = await Product.findOne({
